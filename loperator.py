bb5169b424c39ffa95dc8e107a153ea063a8af5a
Daniel Bolgheroni <dbolgheroni@gmail.com>
2016-02-22 15:58:29 -0300

    Add 3 queues (free op, busy op and calls).
    Every call is added to the queue, and only removed if there is a
    operator ready to answer it. If there is not enough operators to answer
    the calls.
    
    When the operator answer the call, it moves from the free queue to the
    busy queue. The vice-versa operation, when the caller disconnects, and
    the operator must be returned to the free queue, will be added later.

--- loperator.py
+++ loperator.py
@@ -1,7 +1,25 @@
+from collections import deque
+
 class Operator:
+    freeopq = deque()
+    busyopq = deque()
+
+    # state 0 is free; state 1 is busy
+    # when added, operator is free
     def __init__(self, id=id, state=0):
         self.__id = id
         self.__state = state
+        Operator.freeopq.append(self)
+
+    @classmethod
+    def get_operator(cls):
+        try:
+            op = Operator.freeopq.popleft()
+        except IndexError:
+            return None
+
+        Operator.busyopq.append(op)
+        return op
 
     @property
     def id(self):
--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 
 from __future__ import print_function
 
+from collections import deque
+
 from twisted.internet.protocol import Factory
 from twisted.internet import reactor, protocol
 from twisted.protocols.basic import LineReceiver
@@ -16,24 +18,46 @@ class VulcaProtocol(LineReceiver):
 
     def __init__(self, factory):
         self.factory = factory
+        self.__callid = None
 
     # override
     def connectionMade(self):
-        print("connection made")
+        #print("connection made")
+        # receive
         self.transport.write("connect\n\n")
 
     def connectionLost(self, reason):
         print("connection lost")
 
     def lineReceived(self, line):
-        self.parse_callid(line)
+        # parse call uuid
+        self.__callid = self.__parse_callid(line)
 
-    def parse_callid(self, line):
+        # when a call uuid is detected, try to get an operator
+        if self.__callid:
+            print("call", self.__callid, "received")
+
+            # add call to queue
+            callq.append(self.__callid)
+            print("call", self.__callid, "waiting in queue")
+
+            # get an operator
+            op = Operator.get_operator()
+            if op:
+                print("call", self.__callid, "answered by operator",
+                        op.id)
+
+                # if there is an operator, call is popped from queue
+                callq.popleft()
+            else:
+                print("all operators are busy")
+
+    def __parse_callid(self, line):
         # try not to use regex, since it's more complex and slower
         callid = line.split(': ')
 
         if callid[0] == 'variable_call_uuid':
-            print('call', callid[1], 'received')
+            return callid[1]
 
 
 class VulcaFactory(Factory):
@@ -42,9 +66,11 @@ class VulcaFactory(Factory):
 
 
 if __name__ == '__main__':
-    # state 0 is free; state 1 is busy
     o1 = Operator(id=0)
     o2 = Operator(id=1)
+
+    # it's a common idiom to use deque() as a queue
+    callq = deque()
 
     reactor.listenTCP(5678, VulcaFactory())
     reactor.run()
