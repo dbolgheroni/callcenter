0ff152192bb0b1c6596a63a7978dafdcce928344
Daniel Bolgheroni <dbolgheroni@gmail.com>
2016-02-28 19:18:17 -0300

    Add property.

--- loperator.py
+++ loperator.py
@@ -6,8 +6,9 @@ class Operator:
     availableq = defer.DeferredQueue()
 
     # when added, operator is free
-    def __init__(self, id=id, state=0):
+    def __init__(self, id=999, effid=1000, state=0):
         self.__id = id
+        self.__effid = effid
         self.__state = state
         Operator.availableq.put(self)
 
@@ -26,3 +27,11 @@ class Operator:
     @id.setter
     def id(self, id):
         self.__id = id
+
+    @property
+    def effid(self):
+        return self.__effid
+
+    @effid.setter
+    def effid(self, effid):
+        self.__effid = effid
